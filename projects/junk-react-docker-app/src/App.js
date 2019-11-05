a9711e93f7bb316e66702ebc6b0098392a1d2e88
Idan Beck <idanbeck@gmail.com>
2019-11-05 15:42:54 -0800

    sync

--- projects/junk-react-docker-app/src/App.js
+++ projects/junk-react-docker-app/src/App.js
@@ -11,7 +11,7 @@ class App extends Component {
           <h2>Welcome to React</h2>
         </div>
         <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload sdf
+          To get started, edit <code>src/App.js</code> and save to reload h sds ihihi ppp
 
           Based on <a href="https://mherman.org/blog/dockerizing-a-react-app/">https://mherman.org/blog/dockerizing-a-react-app/</a>
         </p>
