b1fe2e7adca451b1d7242d11f0470802c21a2bbb
Idan Beck <idanbeck@gmail.com>
2020-01-27 02:53:10 +0800

    moving shit

---
+++ projects/rustbook/chapter4/target/debug/deps/chapter4-3bfd7391f8c9a9d4.d
@@ -0,0 +1,5 @@
+/Users/idanbeck/dev/junkbox/projects/chapter4/target/debug/deps/chapter4-3bfd7391f8c9a9d4: src/main.rs
+
+/Users/idanbeck/dev/junkbox/projects/chapter4/target/debug/deps/chapter4-3bfd7391f8c9a9d4.d: src/main.rs
+
+src/main.rs:
