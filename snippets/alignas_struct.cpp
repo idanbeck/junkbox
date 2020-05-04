b137df285d83a37a5440409e4cd2b0ddee04b916
Idan Beck <idanbeck@gmail.com>
2020-05-04 13:49:41 -0700

    bla

---
+++ snippets/alignas_struct.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+alignas (8) struct mystruct {
+		char a;
+		char b;
+		alignas(4) char c[3];
+};
+
+int main(int argc, char *argv[]) {
+	struct mystruct t;
+	printf("sizeof mystruct: %d sizeof t: %d\n", sizeof(struct mystruct), sizeof(t));
+
+	return 0;
+}
