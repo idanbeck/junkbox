6d9e274f60895d4ddd26ff6bd96fc57b5d494a8f
Idan Beck <idanbeck@gmail.com>
2020-09-11 14:39:28 -0700

    sync

--- study/cs234/assignment1/vi_and_pi.py
+++ study/cs234/assignment1/vi_and_pi.py
@@ -262,18 +262,18 @@ if __name__ == "__main__":
 
 	# comment/uncomment these lines to switch between deterministic/stochastic environments
 	#env = gym.make("Deterministic-4x4-FrozenLake-v0")
-	env = gym.make("Stochastic-4x4-FrozenLake-v0")
+	#env = gym.make("Stochastic-4x4-FrozenLake-v0")
 	#env = gym.make("Deterministic-8x8-FrozenLake-v0")
-	#env = gym.make("Stochastic-8x8-FrozenLake-v0")
+	env = gym.make("Stochastic-8x8-FrozenLake-v0")
 
 	print("\n" + "-"*25 + "\nBeginning Policy Iteration\n" + "-"*25)
 
 	V_pi, p_pi = policy_iteration(env.P, env.nS, env.nA, gamma=0.9, tol=1e-3)
-	render_single(env, p_pi, 100)
+	render_single(env, p_pi, 200)
 
 	print("\n" + "-"*25 + "\nBeginning Value Iteration\n" + "-"*25)
 
 	V_vi, p_vi = value_iteration(env.P, env.nS, env.nA, gamma=0.9, tol=1e-3)
-	render_single(env, p_vi, 100)
+	render_single(env, p_vi, 200)
 
 
