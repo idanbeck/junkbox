db9fc04cfb88b637e38bdf1e667d445744c96b92
Idan Beck <idanbeck@gmail.com>
2020-09-11 14:27:09 -0700

    cs234 hw1

---
+++ study/cs234/setup-virtual-env.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+
+# cd assignment1
+sudo pip install virtualenv      # This may already be installed
+virtualenv .env                  # Create a virtual environment
+source .env/bin/activate         # Activate the virtual environment
+pip install -r requirements.txt  # Install dependencies
+# Work on the assignment for a while ...
+deactivate                       # Exit the virtual environment
